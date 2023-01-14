11a225c2c08f11948b7e810ffcfdde22cf40c557
OOKucherenko <okld52dec@gmail.com>
2023-01-14 03:08:02 +0200

    Завдання 1 та завдання 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const containerGallery = document.querySelector('.gallery');
+const imgMarkup = createImages(galleryItems);
+containerGallery.insertAdjacentHTML('beforeend', imgMarkup);
+
+function createImages(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `
+            <div class="gallery__item">
+              <a class="gallery__link" href="${original}">
+                <img
+                  class="gallery__image"
+                  src="${preview}"
+                  data-source="${original}"
+                  alt="${description}"
+                />
+              </a>
+            </div>
+            `;
+    })
+    .join('');
+}
+containerGallery.addEventListener('click', onImageClick);
+function onImageClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  const originalImage = e.target.dataset.source;
+  modalImg(originalImage);
+}
+
+function modalImg(link) {
+  const instance = basicLightbox.create(`<img src="${link}">`, {
+    onShow: instance => window.addEventListener('keydown', onEscapePress),
+    onClose: instance => window.removeEventListener('keydown', onEscapePress),
+  });
+  instance.show();
+
+  function onEscapePress(event) {
+    if (event.key === 'Escape') {
+      instance.close();
+      window.removeEventListener('keydown', onEscapePress);
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const containerGallery = document.querySelector('.gallery');
+const imgMarkup = createImages(galleryItems);
+containerGallery.insertAdjacentHTML('beforeend', imgMarkup);
+
+function createImages(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});
